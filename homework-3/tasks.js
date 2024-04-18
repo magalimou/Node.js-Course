742e30098c927fc0d58422643c50d7a878057db2
magali <maguimourino@gmail.com>
2024-04-18 03:45:42 -0300

    task 1 completed

--- homework-3/tasks.js
+++ homework-3/tasks.js
@@ -1,9 +1,39 @@
 /*Task 1: Immutability and Pure Functions*/
-
-function calculateDiscountedPrice(products[], discount)
+function calculateDiscountedPrice(products, discount)
 {
+    let discountedProducts = [];
+    
+    if(discount > 0 && discount <= 100){
+        for(p of products){   
+            let discountedPrice = p.price - ((p.price * discount) / 100);
+            discountedProducts.push({name: p.name, price: discountedPrice});
+        }
+        
+    }else{
+        throw new Error("Discount should be between 0 and 100");
+    }
 
+    return discountedProducts;
 }
 
-/*The function should return a new array of products with discounted prices based 
-on the given percentage, without modifying the original products.*/
+function calculateTotalPrice(products)
+{
+    let totalPrice = 0;
+    for(p of products){
+        totalPrice = totalPrice + p.price;
+    }
+    return totalPrice;
+}
+
+/*Testing functions task 1*/
+let products = [
+    { name: 'Product 1', price: 100 },
+    { name: 'Product 2', price: 250 },
+    { name: 'Product 3', price: 400 }
+  ];
+
+let discountedProducts = calculateDiscountedPrice(products, 10);
+console.log(discountedProducts);
+
+let totalPrice = calculateTotalPrice(products);
+console.log(totalPrice);
