2f00d94377ae22f663e0d28cd3fb7b28ca958bde
magali <maguimourino@gmail.com>
2024-04-18 03:35:10 -0300

    folder

---
+++ homework-3/tasks.js
@@ -0,0 +1,9 @@
+/*Task 1: Immutability and Pure Functions*/
+
+function calculateDiscountedPrice(products[], discount)
+{
+
+}
+
+/*The function should return a new array of products with discounted prices based 
+on the given percentage, without modifying the original products.*/
