c535cca00cf9f1ce15d4b24617d42d3bab432140
magali <maguimourino@gmail.com>
2024-04-24 23:40:57 -0300

    task 7 completed

--- homework-4/task7.js
+++ homework-4/task7.js
@@ -1,7 +1,51 @@
 /**Task 7: Object Property Validation**/
 
-
-/*Implement a function called validateObject that takes an object and a validation schema as arguments. The schema should define the required properties, their types, and any additional validation rules. The function should return true if the object matches the schema, and false otherwise. You can choose any schema you want.*/
 function validateObject(obj, schema){
-    
+
+    for(let key in schema)
+    {
+        if(schema[key].required && !obj.hasOwnProperty(key))
+        {
+            return false;
+        }
+        if(typeof obj[key] !== schema[key].type)
+        {
+            return false;
+        }
+    }
+    return true;
 }
+
+let schema = {
+    name: {
+        type: 'string',
+        required: true
+    },
+    age: {
+        type: 'number',
+        required: true
+    },
+}
+
+let obj = {
+    name: 'Julia',
+    age: 35,
+}
+
+console.log(validateObject(obj, schema)); 
+
+let obj2 = {
+    name: 4,
+    age: 'julia',
+}
+
+console.log(validateObject(obj2, schema));
+
+let obj3 = {
+    country: 'USA',
+    city: 'New York',
+}
+
+console.log(validateObject(obj3, schema));
+
+
