f51ac1a2e38b79050ebc74eed596e6f7d88ba455
magali <maguimourino@gmail.com>
2024-04-30 13:21:58 -0300

    fix task 1

--- homework-5/task1.js
+++ homework-5/task1.js
@@ -1,26 +1,45 @@
 /*Task 1: Advanced Array Filtering*/
 
 function customFilterUnique(arr, callback) {
-   let unique = [];
+   let result = [];
+   let unique = new Set();
    for(let i of arr){
       if(callback(i)){
-         if(!unique.includes(i)){
-            unique.push(i);
+         let objString = JSON.stringify(i);
+         if(!unique.has(objString)){
+            unique.add(objString);
+            result.push(i);
          }
       }
    }
-   return unique;
+   return result;
 }
 
-// Example
+// Example 1
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const callback = (item) => item % 2 === 0;
 let result = customFilterUnique(arr, callback);
 console.log(result);
 
-/*Use the customFilterUnique function to filter an array of objects based on a specific property and return only unique objects.*/
-
+//Example 2
 let array2 = [{name: 'John', age: 25}, {name: 'Jane', age: 30}, {name: 'Anne', age: 25}, {name: 'Frank', age: 26}];
 const callback2 = (item) => item.age === 25;
 
-console.log(customFilterUnique(array2, callback2)); 
+console.log(customFilterUnique(array2, callback2));
+
+//Example 3
+let arrayOfObjects = [
+   { a: 1, b: 2 },
+   { a: 1, b: 2 },
+   { c: 3, d: 4 },
+   { a: 5, b: 6 },
+   { e: 7, f: 8 },
+   { g: 9, h: 0 }
+ ];
+ 
+ const filterByPropertyA = (obj) => {
+   if (!obj || typeof obj !== "object") throw new Error("pass an object as parameter")
+   return Object.keys(obj).includes("a") ? obj : undefined
+ }
+ 
+ console.log(customFilterUnique(arrayOfObjects, filterByPropertyA))
