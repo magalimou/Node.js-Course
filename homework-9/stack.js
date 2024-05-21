74fdaae477d4b03d17a637c122cad46781d924b5
magali <maguimourino@gmail.com>
2024-05-21 01:54:46 -0300

    min max stack

--- homework-9/stack.js
+++ homework-9/stack.js
@@ -36,6 +36,48 @@ class Stack {
     }
 }
 
+
+class MinMaxStack {
+	#stack;
+	#minStack;
+	#maxStack;
+
+    constructor() {
+        this.#stack = new Stack();
+        this.#minStack = new Stack();
+        this.#maxStack = new Stack();
+    }
+    
+    push(element){
+        this.#stack.push(element);
+        if(this.#minStack.isEmpty() || element <= this.#minStack.peek()){
+            this.#minStack.push(element);
+        }
+        if(this.#maxStack.isEmpty() || element >= this.#maxStack.peek()){
+            this.#maxStack.push(element);
+        }
+    }
+
+    pop(){
+        const element = this.#stack.pop();
+        if(element === this.#minStack.peek()){
+            this.#minStack.pop();
+        }
+        if(element === this.#maxStack.peek()){
+            this.#maxStack.pop();
+        }
+        return element;
+    }
+
+    getMin(){
+        return this.#minStack.peek();
+    }
+
+    getMax(){
+        return this.#maxStack.peek();
+    }
+}
+
 // Usage
 const stack = new Stack();
 stack.push(1);
@@ -47,5 +89,18 @@ console.log(stack.peek());
 console.log(stack.isEmpty());
 console.log(stack.size());
 
+// Usage for MinMaxStack
+const minMaxStack = new MinMaxStack();
+minMaxStack.push(5);
+minMaxStack.push(2);
+minMaxStack.push(7);
+minMaxStack.push(3);
+
+console.log(minMaxStack.getMin());
+console.log(minMaxStack.getMax());
+
+minMaxStack.pop();
+console.log(minMaxStack.getMin());
+console.log(minMaxStack.getMax());
 
   
