752e993a14a785ee1f1197e003123225d7fcc6f2
magali <maguimourino@gmail.com>
2024-06-01 13:41:36 -0300

    .

---
+++ homework-11/task.js
@@ -0,0 +1,8 @@
+function myJSONParse(jsonString) {
+    // Implement JSON parsing with regular expressions...
+  }
+  
+  const jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+  const jsonObject = myJSONParse(jsonString);
+  
+  console.log(jsonObject); // Should output the parsed JavaScript object.
